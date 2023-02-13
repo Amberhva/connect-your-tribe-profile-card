701cfef4bb49bc6bc946623133bc2c5d9d109bcd
Amberhva <amber.van.der.reijden@hva.nl>
2023-02-13 11:06:52 +0100

    Update
    Added my bio

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 // Importeer express uit de node_modules map
-import express, { response } from 'express'
+import express, { response } from "express";
 
 // Maak een url aan van je whoisfdnd
-const url = 'https://whois.fdnd.nl/api/v1/member/amber-van-der-reijden'
+const url = "https://whois.fdnd.nl/api/v1/member/amber-van-der-reijden";
 
 // Fetch de data van de url
-const data = await fetch(url).then((response) => response.json())
+const data = await fetch(url).then((response) => response.json());
 
-// console.log(data.member)
+// console.log(data.member.bio);
 
 // Maak een nieuwe express app aan
-const app = express()
+const app = express();
 
 // Stel ejs in als template engine en geef de 'views' map door
-app.set('view engine', 'ejs')
-app.set('views', './views')
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 // Gebruik de map 'public' voor statische resources
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // Maak een route voor de index
-app.get('/', function (req, res) {
-  // res.send('Hello World!')
-  res.render('index', data)
-})
+app.get("/", function (req, res) {
+    // res.send('Hello World!')
+    res.render("index", data);
+});
 
 // Stel het poortnummer in waar express op gaat luisteren
-app.set('port', process.env.PORT || 8000)
+app.set("port", process.env.PORT || 8000);
 
 // Start express op, haal het ingestelde poortnummer op
-app.listen(app.get('port'), function () {
-  // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+app.listen(app.get("port"), function () {
+    // Toon een bericht in de console en geef het poortnummer door
+    console.log(`Application started on http://localhost:${app.get("port")}`);
+});
